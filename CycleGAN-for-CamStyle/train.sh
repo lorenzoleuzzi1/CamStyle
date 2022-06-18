583b619aa82e6c5ad963c49defa5f92b29f39c99
lorenzoleuzzi1 <106617377+lorenzoleuzzi1@users.noreply.github.com>
2022-06-18 16:37:43 +0200

    Update train.sh

--- CycleGAN-for-CamStyle/train.sh
+++ CycleGAN-for-CamStyle/train.sh
@@ -3,7 +3,7 @@ pip install -r /content/pytorch-CycleGAN-and-pix2pix/requirements.txt
 git clone https://github.com/lorenzoleuzzi1/CamStyle
 pip install -r /content/CamStyle/CycleGAN-for-CamStyle/requirements.txt
 pip install scipy==1.2.1
-python -m visdom.server
+#python -m visdom.server
 
 
 sh train_market.sh
