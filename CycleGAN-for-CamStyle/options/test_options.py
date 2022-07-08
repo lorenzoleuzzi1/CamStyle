88b9bec4a5d522bf47fb432472910cbd7aad946c
lorenzoleuzzi1 <106617377+lorenzoleuzzi1@users.noreply.github.com>
2022-07-08 18:21:34 +0200

    Update test_options.py

--- CycleGAN-for-CamStyle/options/test_options.py
+++ CycleGAN-for-CamStyle/options/test_options.py
@@ -9,7 +9,7 @@ class TestOptions(BaseOptions):
         parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
         parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
         parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
-        parser.add_argument('--how_many', type=int, default=500, help='how many test images to run')
+        parser.add_argument('--how_many', type=int, default=700, help='how many test images to run')
         parser.add_argument('--save_root', type=str, default='results/market/bounding_box_train_camstyle')
         self.isTrain = False
         return parser
