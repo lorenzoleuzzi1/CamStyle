694fcffc67b67ae663a0d13956a0407e28a7698b
lorenzoleuzzi1 <106617377+lorenzoleuzzi1@users.noreply.github.com>
2022-07-11 13:03:58 +0200

    Update train_market.sh

--- CycleGAN-for-CamStyle/train_market.sh
+++ CycleGAN-for-CamStyle/train_market.sh
@@ -5,27 +5,27 @@ python train.py --dataroot ./datasets/market --name market-c1-c3 --camA 1 --camB
 python train.py --dataroot ./datasets/market --name market-c1-c4 --camA 1 --camB 4
 python train.py --dataroot ./datasets/market --name market-c1-c5 --camA 1 --camB 5
 python train.py --dataroot ./datasets/market --name market-c1-c6 --camA 1 --camB 6
-python train.py --dataroot ./datasets/market --name market-c1-c6 --camA 1 --camB 7
-python train.py --dataroot ./datasets/market --name market-c1-c6 --camA 1 --camB 8
+python train.py --dataroot ./datasets/market --name market-c1-c7 --camA 1 --camB 7
+python train.py --dataroot ./datasets/market --name market-c1-c8 --camA 1 --camB 8
 
 python train.py --dataroot ./datasets/market --name market-c2-c3 --camA 2 --camB 3
 python train.py --dataroot ./datasets/market --name market-c2-c4 --camA 2 --camB 4
 python train.py --dataroot ./datasets/market --name market-c2-c5 --camA 2 --camB 5
 python train.py --dataroot ./datasets/market --name market-c2-c6 --camA 2 --camB 6
-python train.py --dataroot ./datasets/market --name market-c2-c5 --camA 2 --camB 7
-python train.py --dataroot ./datasets/market --name market-c2-c6 --camA 2 --camB 8
+python train.py --dataroot ./datasets/market --name market-c2-c7 --camA 2 --camB 7
+python train.py --dataroot ./datasets/market --name market-c2-c8 --camA 2 --camB 8
 
 python train.py --dataroot ./datasets/market --name market-c3-c4 --camA 3 --camB 4
 python train.py --dataroot ./datasets/market --name market-c3-c5 --camA 3 --camB 5
 python train.py --dataroot ./datasets/market --name market-c3-c6 --camA 3 --camB 6
-python train.py --dataroot ./datasets/market --name market-c2-c5 --camA 3 --camB 7
-python train.py --dataroot ./datasets/market --name market-c2-c6 --camA 3 --camB 8
+python train.py --dataroot ./datasets/market --name market-c2-c7 --camA 3 --camB 7
+python train.py --dataroot ./datasets/market --name market-c2-c8 --camA 3 --camB 8
 
 python train.py --dataroot ./datasets/market --name market-c4-c5 --camA 4 --camB 5
 python train.py --dataroot ./datasets/market --name market-c4-c6 --camA 4 --camB 6
-python train.py --dataroot ./datasets/market --name market-c4-c5 --camA 4 --camB 7
-python train.py --dataroot ./datasets/market --name market-c4-c6 --camA 4 --camB 8
+python train.py --dataroot ./datasets/market --name market-c4-c7 --camA 4 --camB 7
+python train.py --dataroot ./datasets/market --name market-c4-c8 --camA 4 --camB 8
 
 python train.py --dataroot ./datasets/market --name market-c5-c6 --camA 5 --camB 6
-python train.py --dataroot ./datasets/market --name market-c5-c6 --camA 5 --camB 7
-python train.py --dataroot ./datasets/market --name market-c5-c6 --camA 5 --camB 8
+python train.py --dataroot ./datasets/market --name market-c5-c7 --camA 5 --camB 7
+python train.py --dataroot ./datasets/market --name market-c5-c8 --camA 5 --camB 8
